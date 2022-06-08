bf87e382497615ef6c2cd61d1cea472c79b8dfc1
Gontyjo <rodrigoarniqueira@gmail.com>
2022-06-08 18:53:57 -0300

    updated test operations, created and tested new filters

--- api-collections/src/main/kotlin/org/teste/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/org/teste/TesteOperacoes.kt
@@ -8,10 +8,10 @@ fun main() {
         println(salario)
     }
 
-    println("MEDIA------------------------------")
+    println("OPERACOES--------------------------")
     println("Media salarios: ${salarios.average()}")
-
-
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
 
     println("FILTERS----------------------------")
     salarios.forEach{ if(it > 2500.0){
@@ -20,4 +20,15 @@ fun main() {
     println("FILTERS----------------------------")
     val salariosMaiorque2500 = salarios.filter { it > 2500.0 }
     salariosMaiorque2500.forEach { println(it) }
+
+    println("FILTERS----------------------------")
+    println("${salarios.count { it in 2000.0..5000.0}} salarios entre 2000/5000")
+
+    println("FILTERS----------------------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("FILTERS----------------------------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 100.0 })
 }
