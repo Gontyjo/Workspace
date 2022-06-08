07bc648d81c267b0417a6c776610ca34e82184f7
Gontyjo <rodrigoarniqueira@gmail.com>
2022-06-08 19:29:16 -0300

    updated test list, extract employee to separated file and created test set

--- api-collections/src/main/kotlin/org/teste/TesteList.kt
+++ api-collections/src/main/kotlin/org/teste/TesteList.kt
@@ -27,17 +27,3 @@ fun main() {
         .sortedByDescending { it.salario }
         .forEach{ println(it)}
 }
-
-data class Funcionario(
-    val nome:String,
-    val salario:Double,
-    val tipoContratacao:String,
-){
-    override fun toString(): String =
-            """
-            -----------
-            Nome: $nome
-            Salario: $salario
-            -----------
-            """.trimIndent()
-}
