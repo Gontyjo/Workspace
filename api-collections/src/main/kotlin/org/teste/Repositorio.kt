1c7a2cb2ba251dfea30ad12885a86cfb16de418d
Gontyjo <rodrigoarniqueira@gmail.com>
2022-06-09 15:48:20 -0300

    created mutableMap test and repositorio class

--- api-collections/src/main/kotlin/org/teste/Repositorio.kt
+++ api-collections/src/main/kotlin/org/teste/Repositorio.kt
@@ -1,4 +1,17 @@
 package org.teste
 
-class Repositorio {
+class Repositorio <T> {
+    private val map = mutableMapOf<String, T>()
+
+    fun create(id: String, value: T){
+        map[id] = value
+    }
+
+    fun findById(id: String) = map[id]
+
+    fun findAll() = map.values
+
+    fun remove(id: String){
+        map.remove(id)
+    }
 }
